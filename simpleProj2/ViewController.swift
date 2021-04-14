389e65ba95a5efa3c6d9e79e192a5a50768605ba
7se7olod <pushin_95@mail.ru>
2021-04-14 17:04:58 +0400

    Initial Commit

---
+++ simpleProj2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleProj2
+//
+//  Created by Всеволод on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
